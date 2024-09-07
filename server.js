746e8225790a0d8edd410d258eddfc58a82dcee5
tommo593 <tomdevwork@gmail.com>
2024-09-07 15:59:05 +0100

    Stuff

--- server.js
+++ server.js
@@ -1,23 +1,54 @@
-var express = require("express")
-var { createHandler } = require("graphql-http/lib/use/express")
-var { buildSchema } = require("graphql")
- 
+var express = require("express");
+var cors = require("cors"); // Import the cors package
+var { createHandler } = require("graphql-http/lib/use/express");
+var { buildSchema } = require("graphql");
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     hello: String
+    getPokemon(pokemon: String!): Pokemon
   }
-`)
- 
+
+  type Pokemon {
+    sprite: String
+    num: Int
+    species: String
+    color: String
+  }
+`);
+
+// Sample data for demonstration purposes
+const pokemonData = {
+  dragonite: {
+    sprite: "https://img.pokemondb.net/sprites/sword-shield/normal/dragonite.png",
+    num: 149,
+    species: "Dragonite",
+    color: "Yellow",
+  },
+  pikachu: {
+    sprite: "https://img.pokemondb.net/sprites/sword-shield/normal/pikachu.png",
+    num: 25,
+    species: "Pikachu",
+    color: "Yellow",
+  },
+};
+
 // The root provides a resolver function for each API endpoint
 var root = {
   hello() {
-    return "Hello world!"
+    return "Hello world!";
   },
-}
- 
-var app = express()
- 
+  getPokemon({ pokemon }) {
+    return pokemonData[pokemon.toLowerCase()] || null;
+  },
+};
+
+var app = express();
+
+// Use CORS middleware
+app.use(cors());
+
 // Create and use the GraphQL handler.
 app.all(
   "/graphql",
@@ -25,8 +56,9 @@ app.all(
     schema: schema,
     rootValue: root,
   })
-)
- 
+);
+
 // Start the server at port
-app.listen(4000)
-console.log("Running a GraphQL API server at http://localhost:4000/graphql")
+app.listen(3147, () => {
+  console.log("Running a GraphQL API server at http://localhost:3147");
+});
