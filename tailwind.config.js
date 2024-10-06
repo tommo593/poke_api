52032b2ddb9122c726155628c44f5a44bc350914
tommo593 <tomdevwork@gmail.com>
2024-10-06 17:50:14 +0100

    Added breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '490px',
+        md: '768px',
+        lg: '1050px',
+        xl: '1440px',
+      },
+    },
   },
   plugins: [],
 };
